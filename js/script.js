a8003f51a2723818ee4e0c9354033c34304d6d9f
Rebecca Horwitz <rebeccahorwitz@Rebeccas-MacBook-Pro.local>
2017-07-21 17:57:08 -0700

    added details to comments and fixed input text issues

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
-// Keypress Event Listener
+// Keypress Event Listener with function(event)
 document.addEventListener("keypress", function(event) {
+  console.log(this, "event listener");
   console.log(event.which);
 });
 
-// Function for changing character to keycode
+// Function for changing character to keycode when typed inside of input tag in html
 function typedKey(event) {
   var key = event.which || event.keyCode;
   document.getElementById('key-value').innerHTML = key;
+  console.log(this, "this is this inside typedKey");
 }
 
 
